00223e8296531a4971bb8bd75dbe017982702915
Jessie Chatham Spencer <jessie@teainspace.com>
2023-03-24 07:29:54 +0100

    Parameterize object

--- produce_data.py
+++ produce_data.py
@@ -4,6 +4,7 @@ from minio import Minio
 s3_endpoint = os.environ.get("S3_ENDPOINT")
 s3_access_key= os.environ.get("S3_ACCESS_KEY")
 s3_secret_key= os.environ.get("S3_SECRET_KEY")
+object_key = os.environ.get("S3_OBJECT")
 
 client = Minio(
     s3_endpoint,
@@ -11,9 +12,9 @@ client = Minio(
     secret_key=s3_secret_key,
 )
 
+print("object: ", object_key)
+
 os.makedirs("data", exist_ok=True)
 
-client.fget_object("amedemo", "mixed_0101_gradual.csv", "mixed_0101_gradual.csv")
+client.fget_object("amedemo", "mixed_0101_gradual.csv", "data/mixed_0101_gradual.csv")
 
-with open(file="data/output.txt", mode="w") as download_file:
- download_file.write("this is some data")
